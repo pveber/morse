93dc6a2343347d5016ebf023ebbc4d6accdaa9da
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-04-29 08:56:46 +0200

    Revert "remove idCreate function"
    This reverts commit a21b9231f5664fbab38b896dec8345b22fb1b4aa.

--- R/survDataCheck.R
+++ R/survDataCheck.R
@@ -116,8 +116,7 @@ survDataCheck <- function(data, diagnosis.plot = TRUE) {
   ##
   ## 7 assert each (replicate, concentration, time) triplet is unique
   ##
-  ID <- str_c(data[, "replicate"], data[, "conc"], data[, "time"],
-                          sep = "_")
+  ID <- idCreate(data, notime = FALSE) # ID vector
   if (any(duplicated(ID))) {
     msg <- paste("The (replicate, conc, time) triplet ",
                  ID[duplicated(ID)],
