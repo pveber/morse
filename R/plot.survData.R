57f60d7accae1bc2316dcafc81a00d63fe2ba48a
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-04-30 09:21:32 +0200

    remove ggplot2 from imports

--- R/plot.survData.R
+++ R/plot.survData.R
@@ -137,7 +137,6 @@ survDataPlotFullLattice <- function(data, xlab, ylab, addlegend) {
   }
 }
 
-#' @import ggplot2
 survDataPlotFullGG <- function(data, xlab, ylab, addlegend) {
   # plot of survival data: one subplot for each concentration, and one color for
   # each replicate for ggplot graphics
@@ -206,8 +205,6 @@ survDataPlotFull <- function(data,
 #'
 #'#' @examples
 #'
-#' library(ggplot2)
-#'
 #' # (1) Load the data
 #' data(zinc)
 #' zinc <- survData(zinc)
@@ -227,7 +224,6 @@ survDataPlotFull <- function(data,
 
 #' @export
 #'
-#' @import ggplot2
 #' @import grDevices
 # FIXME: delete imports if really not needed
 # @importFrom gridExtra grid.arrange arrangeGrob
