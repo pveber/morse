4b7f62f7897d4a386754e59e78f0dfc96730fee4
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-06-22 11:34:36 +0200

    Revert "modif reproLlmCI for ppc med"
    This reverts commit 04e05e097eebebe19938c26d3248221a5fe151c3.

--- R/plot.reproFitTT.R
+++ R/plot.reproFitTT.R
@@ -37,7 +37,6 @@ reproLlmCI <- function(fit, x) {
   # quantiles
   qinf95 = NULL
   qsup95 = NULL
-  qmed = NULL
   
   # poisson
   if (fit$model.label == "P") {
@@ -46,7 +45,6 @@ reproLlmCI <- function(fit, x) {
       # IC 95%
       qinf95[i] <- quantile(theomean, probs = 0.025, na.rm = TRUE)
       qsup95[i] <- quantile(theomean, probs = 0.975, na.rm = TRUE)
-      qmed[i] <- quantile(theomean, probs = 0.5, na.rm = TRUE)
     }
   }
   
@@ -62,13 +60,11 @@ reproLlmCI <- function(fit, x) {
       # IC 95%
       qinf95[i] <- quantile(theo, probs = 0.025, na.rm = TRUE)
       qsup95[i] <- quantile(theo, probs = 0.975, na.rm = TRUE)
-      qmed[i] <- quantile(theo, probs = 0.5, na.rm = TRUE)
     }
   }
   # values for CI
   ci <- list(qinf95 = qinf95,
-             qsup95 = qsup95,
-             qmed = qmed)
+             qsup95 = qsup95)
   
   return(ci)
 }
