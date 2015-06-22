2a846f1dc42d72cd838f82ee3311e543b8cec4a8
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-06-22 15:05:29 +0200

    exemple

--- R/ppc.R
+++ R/ppc.R
@@ -118,6 +118,26 @@ reproPpc <- function(x, data, style) {
 #' @param x An object of class \code{survFitTT} or \code{reproFitTT}
 #' @param style generic or ggplot
 #' 
+#' @examples
+#' 
+#' # (1) Load the data
+#' data(cadmium1)
+#' 
+#' # (2) Create an object of class "reproData"
+#' dat <- reproData(cadmium1)
+#' 
+#' \dontrun{
+#' # (3) Run the reproFitTT function with the log-logistic gamma-poisson model
+#' out <- reproFitTT(dat2, stoc.part = "gammapoisson", 
+#' ecx = c(5, 10, 15, 20, 30, 50, 80), quiet = TRUE)
+#' 
+#' # (4) Plot the observed vs predicted values with generic style
+#' ppc(out)
+#' 
+#' # (4) Plot the observed vs predicted values with ggplot2 style
+#' ppc(out, style = "ggplot")
+#' }
+#' 
 #' @export
 #' @import ggplot2
 #' 
