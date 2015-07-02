8aa47ae42b3ece5a9332aea0c747fd4e4a7770a0
philipperuiz <philippe.ruiz@univ-lyon1.fr>
2015-07-02 10:58:49 +0200

    addlegendFalse

--- R/plot.reproFitTT.R
+++ R/plot.reproFitTT.R
@@ -354,7 +354,7 @@ plot.reproFitTT <- function(x,
                             cicol,
                             cilty,
                             cilwd,
-                            addlegend = TRUE,
+                            addlegend = FALSE,
                             log.scale = FALSE,
                             style = "generic",
                             ...) {
