42be376cd13abf3b6030591eddb78e1890f8b7ad
philipperuiz <philippe.ruiz@univ-lyon1.fr>
2015-12-03 17:13:18 +0100

    Y-axis sart to 0 for all survData plot  in ggplot style

--- R/plot.survData.R
+++ R/plot.survData.R
@@ -216,6 +216,7 @@ dataPlotFullGG <- function(data, xlab, ylab, resp, addlegend, remove.someLabels)
                          }
                        ) +
     scale_y_continuous(breaks = unique(round(pretty(c(0, max(data[, resp])))))) +
+    expand_limits(x = 0, y = 0) +
     theme_minimal()
 
   # legend option
@@ -329,7 +330,8 @@ survDataPlotTargetTime <- function(x, xlab, ylab, main, target.time,
                            df$display.conc
                          }
       ) +
-      scale_y_continuous(breaks = unique(round(pretty(c(0, max(df$Nsurv))))))
+      scale_y_continuous(breaks = unique(round(pretty(c(0, max(df$Nsurv)))))) +
+      expand_limits(x = 0, y = 0)
 
     # legend option
     if (addlegend) {
