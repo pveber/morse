2d63970b2b52ae101444b7623d61a80eddf841d7
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-12-14 10:29:27 +0100

    update example survFitTT

--- R/survFitTT.R
+++ R/survFitTT.R
@@ -100,11 +100,6 @@
 #' require(ggplot2)
 #' plot(out, xlab = expression("Concentration in" ~ mu~g.L^{-1}),
 #'      fitcol = "blue", ci = TRUE, cicol = "blue",  style = "ggplot")
-#'
-#' # (7) Add a specific legend with generic type
-#' plot(out, addlegend = FALSE)
-#'      legend("left", legend = c("Without mortality", "With mortality"),
-#'      pch = c(19,1))
 #' }
 #'
 #' @import rjags
