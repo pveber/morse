38486b61877e779e5419c553329bc61f142f2d12
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2015-12-21 09:44:59 +0100

    update version

--- R/morse.R
+++ R/morse.R
@@ -41,7 +41,7 @@
 #' described in the tutorial vignette.
 #'
 #' \tabular{ll}{ Package: \tab morse\cr Type: \tab Package\cr Version: \tab
-#' 2.1.0\cr Date: \tab 2015-12-14\cr License: \tab GPL (>=2)\cr }
+#' 2.1.1\cr Date: \tab 2015-12-21\cr License: \tab GPL (>=2)\cr }
 #'
 #' @name morse-package
 #' @aliases morse-package morse
