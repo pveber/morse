29ab641b3d911bc8853d5427af16df43f07b28cd
Philippe Ruiz <philippe.ruiz@univ-lyon1.fr>
2016-02-08 10:13:14 +0100

    remove deprecated plot

--- R/plot.reproData.R
+++ R/plot.reproData.R
@@ -1,7 +1,7 @@
 #' Plotting method for \code{reproData} objects
 #'
 #' Plots the cumulated number of offspring as a function time (for a fixed
-#' concentration)
+#' concentration).
 #'
 #' @param x an object of class \code{reproData}
 #' @param xlab a title for the \eqn{x}-axis (optional)
@@ -74,124 +74,6 @@ reproDataPlotFull <- function(data, xlab, ylab, style = "generic",
                remove.someLabels)
 }
 
-#' @import ggplot2
-#' @importFrom dplyr filter
-reproDataPlotTargetTime <- function(x,
-                                    xlab,
-                                    ylab,
-                                    main,
-                                    target.time,
-                                    style,
-                                    log.scale,
-                                    addlegend,
-                                    remove.someLabels) {
-
-  if (missing(xlab)) xlab <- "Concentration"
-
-  # plot of cumulated number of offspring as a funtion of concentration
-  # for a fixed time
-
-  if (!target.time %in% x$time)
-    stop("[target.time] is not one of the possible time !")
-
-  # select the target.time
-  xf <- filter(x, x$time == target.time)
-
-  # Selection of datapoints that can be displayed given the type of scale
-  sel <- if(log.scale) xf$conc > 0 else TRUE
-  x <- xf[sel, ]
-  transf_data_conc <- optLogTransform(log.scale, x$conc)
-
-  # Concentration values used for display in linear scale
-  display.conc <- (function() {
-    x <- optLogTransform(log.scale, x$conc)
-    if(log.scale) exp(x) else x
-  })()
-
-  # Define visual parameters
-  mortality <- c(0, 1) # code 0/1 mortality
-  nomortality <- match(x$Nsurv == x$Ninit, c(TRUE, FALSE))
-
-  # without mortality
-  mortality <- mortality[nomortality] # vector of 0 and 1
-
-  # encodes mortality empty dots (1) and not mortality solid dots (19)
-  if (style == "generic") {
-    mortality[which(mortality == 0)] <- 19
-  }
-  if (style == "ggplot") {
-    mortality[which(mortality == 0)] <- "No"
-    mortality[which(mortality == 1)] <- "Yes"
-  }
-
-  # default legend argument
-  legend.position <- "right"
-  legend.title <- "Mortality"
-  legend.name.no <- "No"
-  legend.name.yes <- "Yes"
-
-  # generic
-  if (style == "generic") {
-    plot(transf_data_conc,
-         x$Nreprocumul,
-         xlab = xlab,
-         ylab = ylab,
-         main = main,
-         pch = mortality,
-         yaxt = "n",
-         xaxt = "n")
-    # axis
-    axis(side = 2, at = pretty(c(0, max(x$Nreprocumul))))
-    axis(side = 1, at = transf_data_conc,
-         labels = display.conc)
-
-    # legend
-    if (addlegend) {
-      legend(legend.position,title = legend.title, pch = c(19, 1), bty = "n",
-             legend = c(legend.name.no, legend.name.yes))
-    }
-  }
-
-  #ggplot2
-  if (style == "ggplot") {
-    df <- data.frame(x,
-                     transf_data_conc,
-                     display.conc,
-                     Mortality = mortality)
-
-    # plot
-    gp <- ggplot(df, aes(transf_data_conc, Nreprocumul,
-                         fill = Mortality)) +
-      geom_point(size = 3, pch = 21) +
-      scale_fill_manual(values = if (
-        length(levels(df$Mortality)) == 1 && levels(df$Mortality) == "Yes") {
-        "white"
-        } else if (length(levels(df$Mortality)) == 1 &&
-                   levels(df$Mortality) == "No") {
-          "black"
-        } else {
-          c("black", "white")
-        }) +
-      labs(x = xlab, y = ylab) +
-      ggtitle(main) +
-      theme_minimal() +
-      scale_colour_hue(legend.title, breaks = c("No","Yes"),
-                       labels = c(legend.name.no, legend.name.yes)) +
-      scale_x_continuous(breaks = df$transf_data_conc,
-                         labels = if (remove.someLabels) {
-                           exclude_labels(df$display.conc)
-                         } else {
-                           df$display.conc
-                         })
-
-    if (addlegend) {
-      return(gp)
-    } else {
-      gp <- gp + theme(legend.position = "none")
-      return(gp)
-    }
-  }
-}
 
 reproDataPlotFixedConc <- function(x,
                                    xlab,
