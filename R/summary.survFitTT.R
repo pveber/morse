80a3cc3e7c66e3b2085e03fae7fc159aa55ab390
philipperuiz <philippe.ruiz@univ-lyon1.fr>
2016-03-16 11:06:47 +0100

    best presentation of scientific surv

--- R/summary.survFitTT.R
+++ R/summary.survFitTT.R
@@ -66,15 +66,15 @@ summary.survFitTT <- function(object, quiet = FALSE, ...) {
     res <- rbind(b, e)
   }
 
-  ans1 <- format(round(data.frame(res), digits = 3), scientific = TRUE)
+  ans1 <- format(data.frame(res), scientific = TRUE, digits = 4)
   colnames(ans1) <- c("50%", "2.5%", "97.5%")
 
   # quantiles of estimated model parameters
-  ans2 <- format(round(object$estim.par, digits = 3), scientific = TRUE)
+  ans2 <- format(object$estim.par, scientific = TRUE, digits = 4)
   colnames(ans2) <- c("50%", "2.5%", "97.5%")
 
   # estimated ECx and their CIs 95%
-  ans3 <- format(round(object$estim.LCx, digits = 3), scientific = TRUE)
+  ans3 <- format(object$estim.LCx, scientific = TRUE, digits = 4)
   colnames(ans3) <- c("50%", "2.5%", "97.5%")
 
   # print
