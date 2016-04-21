06d062a484a11a9d14deae66e619dfdfe71fb0a3
philipperuiz <philippe.ruiz@univ-lyon1.fr>
2016-04-21 16:09:37 +0200

    modify jitterObsGenerarator to be use with ppc and DoseResponse [correctCalculJitter]

--- R/morse-internal.R
+++ R/morse-internal.R
@@ -191,11 +191,18 @@ stepCalc <- function(obs_val) {
   return(list(sObs = sObs, stepX = stepX))
 }
 
-jitterObsGenerator <- function(stepX, tab, obs_val, log.scale = FALSE) {
+jitterObsGenerator <- function(stepX, tab, obs_val, ppc = FALSE) {
   # uniform jittering of observed values
-  allSpaceX <- sapply(2:length(stepX),
-                      function(i) { stepX[i] - stepX[i-1] })
-  spaceX <- min(allSpaceX[which(allSpaceX != 0)]) / ifelse(!log.scale, 2, exp(2))
+  if (ppc) {
+    allSpaceX <- sapply(2:length(stepX),
+                        function(i) { stepX[i] - stepX[i-1] })
+    spaceX <- min(allSpaceX[which(allSpaceX != 0)]) / 2
+  } else {
+    allSpaceX <- sapply(2:length(stepX),
+                        function(i) { obs_val[i] - obs_val[i-1] })
+    spaceX <- min(allSpaceX[which(allSpaceX != 0)]) / 4
+  }
+  
   lengthX <- table(tab[, "Obs"])
   jitterObs <- mapply(function(x, y) {
     if (y == 1) {
