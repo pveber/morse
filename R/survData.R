c1f52f920999f1b5e6a168232345faf14da7cc98
Philippe Veber <philippe.veber@gmail.com>
2017-06-27 11:04:12 +0200

    survData_join: fixed doc

--- R/survData.R
+++ R/survData.R
@@ -129,77 +129,68 @@ Ninit <- function(x) {
 }
 
 
-#' Join two datasets and creates a survData object for survival analysis
+#' Joins a concentration and a survival dataset into an argument for 'survData'
 #'
-#' This function joins two datasets and creates a \code{survData} object from
-#' experimental data
-#' provided by two \code{data.frame}. The resulting object
-#' can then be used for plotting and model fitting.
+#' This function joins two datasets, one for exposure measurements, the other
+#' for survival measurements, into a single dataframe that can be used
+#' with the \code{survData} function.
 #'
-#' The \code{data} argument describes experimental results from a survival
-#' assay. Each line of the \code{data.frame}
-#' corresponds to one experimental measurement, that is a number of alive
-#' individuals for a given concentration of pollutant at a certain time
-#' during the assay in a certain replicate. The function fails if
-#' \code{data} does not meet the expected requirements. 
-#' Please run \code{\link{survDataCheck}} to ensure
-#' \code{data} is well-formed.
-#'
-#' @param data a \code{data.frame} containing the following four columns:
+#' @param exposure a \code{data.frame} containing the following three columns:
 #' \itemize{
 #' \item \code{replicate}: a vector of class \code{integer} or factor for replicate
 #' identification
-#' \item \code{conc}: a vector of class \code{numeric} with tested concentrations
-#' (positive values)
 #' \item \code{time}: a vector of class \code{integer} with time points, min value must be 0
 #' \item \code{Nsurv}: a vector of class \code{integer} providing the number of
 #' alive individuals at each time point for each concentration and each replicate
 #' }
-#'
-#' @return A dataframe of class \code{survDataCstC} or class \code{survDataVarC}.
-#'
-#' @keywords transformation
+#' @param concentration a \code{data.frame} containing the following three columns:
+#' \itemize{
+#' \item \code{replicate}: a vector of class \code{integer} or factor for replicate
+#' identification
+#' \item \code{time}: a vector of class \code{integer} with time points, min value must be 0
+#' \item \code{Nsurv}: a vector of class \code{integer} providing the number of
+#' alive individuals at each time point for each concentration and each replicate
+#' }
+#'#'
+#' @return a dataframe suitable for `survData`
 #'
 #' @examples
 #'
 #' # (1) Load the two survival dataset
-#' data(propiconazole_varC_conc) ; data(propiconazole_varC_Nsurv)
-#' 
+#' data(propiconazole_pulse_exposure)
+#' exposure <- propiconazole_pulse_exposure[,c("replicate", "time", "conc")]
+#' survival <- propiconazole_pulse_exposure[,c("replicate", "time", "Nsurv")]
+#'
 #' # (2) Create an objet of class 'survData'
-#' dat_join <- survData_join(propiconazole_varC_conc,propiconazole_varC_Nsurv)
+#' dat_join <- survData(survData_join(exposure, survival))
 #' class(dat_join)
-#' 
-#' @importFrom dplyr left_join rename
-#' 
+#'
 #' @export
-#' 
-survData_join <- function(data_x, data_y){
-  
+#'
+survData_join <- function(exposure, survival) {
+
   ##
-  ## 1 assert column names are correct and assign join data_x or data_y to corresponding data.frame
+  ## 1 assert column names are correct and assign join x or y to corresponding data.frame
   ##
   ref.names_surv <- c("replicate","time","Nsurv")
   ref.names_conc <- c("replicate","time","conc")
-  
+
   ### data_surv
-  if( (all(colnames(data_x) %in% ref.names_surv) & all(colnames(data_y) %in% ref.names_conc))
+  if( (all(colnames(x) %in% ref.names_surv) & all(colnames(y) %in% ref.names_conc))
       ||
-      (all(colnames(data_x) %in% ref.names_conc) & all(colnames(data_y) %in% ref.names_surv))){
-    
-    
-    join_df <- dplyr::full_join(data_x, data_y,
+      (all(colnames(x) %in% ref.names_conc) & all(colnames(y) %in% ref.names_surv))){
+
+
+    join_df <- dplyr::full_join(x, y,
                                 by=c("replicate","time")) %>%
       dplyr::arrange(replicate, time)
-    
-    
+
+
   } else {
-    stop("'data_x' and 'data_y' must both have 3 columns: one with 'replicate','time' and 'Nsurv'
+    stop("'x' and 'y' must both have 3 columns: one with 'replicate','time' and 'Nsurv'
          and the other with 'replicate','time' and 'conc'.")
-    
+
   }
-  
-  # OUT_df <- survData(join_df) 
-  
+
   return(join_df)
-  
 }
