e97766a64886b682e1ac77e9d55ae00d0c60008b
Philippe Veber <philippe.veber@gmail.com>
2017-06-27 09:50:22 +0200

    survData: updated doc

--- R/survData.R
+++ R/survData.R
@@ -5,11 +5,18 @@
 #' can then be used for plotting and model fitting. It can also be used
 #' to generate \emph{individual-time} estimates.
 #'
+#' Survival datasets can be in either constant or variable exposure. The
+#' resulting object, in addition to its \code{survData} class, inherits the
+#' class \code{survDataCstExp} or \code{survDataVarExp} respectively.
+#'
 #' The \code{data} argument describes experimental results from a survival
 #' assay. Each line of the \code{data.frame}
 #' corresponds to one experimental measurement, that is a number of alive
 #' individuals for a given concentration of pollutant at a certain time
-#' during the assay in a certain replicate. The function fails if
+#' during the assay in a certain replicate. Note that either the concentration
+#' or the number of alive individuals may be missing. The dataset is inferred
+#' to be in constant exposure if the concentration is constant for each
+#' replicate and systematically available. The function \code{survData} fails if
 #' \code{data} does not meet the
 #' expected requirements. Please run \code{\link{survDataCheck}} to ensure
 #' \code{data} is well-formed.
@@ -17,12 +24,14 @@
 #' @param data a \code{data.frame} containing the following four columns:
 #' \itemize{
 #' \item \code{replicate}: a vector of class \code{integer} or factor for replicate
-#' identification
+#' identification. A given replicate value should identify the same group of
+#' individuals followed in time
 #' \item \code{conc}: a vector of class \code{numeric} with tested concentrations
-#' (positive values)
+#' (positive values, may contain NAs)
 #' \item \code{time}: a vector of class \code{integer} with time points, min value must be 0
 #' \item \code{Nsurv}: a vector of class \code{integer} providing the number of
 #' alive individuals at each time point for each concentration and each replicate
+#' (may contain NAs)
 #' }
 #'
 #' @return A dataframe of class \code{survData}.
@@ -39,9 +48,9 @@
 #' # (2) Create an objet of class 'survData'
 #' dat <- survData(zinc)
 #' class(dat)
-#' 
+#'
 #' @importFrom dplyr left_join rename
-#' 
+#'
 #' @export
 survData <- function(x) {
 
@@ -74,9 +83,9 @@ survData <- function(x) {
 #'  or \code{FALSE} if the concentration in at least one of the replicates is variable
 #'
 #' @export
-#' 
+#'
 is_exposure_constant <- function(x) {
-  
+
   # Test if concentration is constant in a same replicate
   df.test <- x %>%
     group_by(replicate) %>%
