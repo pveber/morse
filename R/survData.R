2481458ddaa4f3d787676f48b656d3401d25bbe7
Philippe Veber <philippe.veber@gmail.com>
2017-06-27 09:54:07 +0200

    survData: allow additional columns in data.frame
    this is necessary to have an inheritance relation between survData and
    reproData

--- R/survData.R
+++ R/survData.R
@@ -58,13 +58,6 @@ survData <- function(x) {
   if (dim(survDataCheck(x, diagnosis.plot = FALSE))[1] > 0)
     stop("The [x] argument is not well-formed, please use [survDataCheck] for details.")
 
-  x <- data.frame(
-    replicate = x$replicate,
-    time = x$time,
-    conc = x$conc,
-    Nsurv = x$Nsurv
-  )
-
   child_class <-
     if (is_exposure_constant(x)) "survDataCstExp"
     else "survDataVarExp"
