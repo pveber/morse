ee788f718894c65e0689bc82ca78a8699be7580f
vbaudrot <virgile.baudrot@posteo.net>
2017-09-06 12:35:20 +0200

    update tests for LCx

--- tests/testthat/test-LCx.R
+++ tests/testthat/test-LCx.R
@@ -1,23 +1,31 @@
-data("cadmium2")
+# For constant concentration exposure
 
-survDataset <- survData(cadmium2)
-
-survfitDataset_SD <- survFit(survDataset, model_type = "SD")
-survfitDataset_IT <- survFit(survDataset, model_type = "IT")
-
-
-survfitDataset_IT
+load(file = "tests/testdata/fit_cstSD.rda")
+load(file = "tests/testdata/fit_cstIT.rda")
 
 test_that("LCx_survFitCstExp", {
   
-  LCx_IT <- LCx(survfitDataset_IT, X = 50)
-  LCx_SD <- LCx(survfitDataset_SD, X = 50)
+  LCx_cstIT <- LCx(fit_cstIT, X = 50)
+  LCx_cstSD <- LCx(fit_cstSD, X = 50)
 
-  expect_is(LCx_IT, "list")
-  expect_is(LCx_SD, "list")
+  expect_is(LCx_cstIT, "list")
+  expect_is(LCx_cstSD, "list")
 
 })
 
+# For time varying concentration
 
+load(file = "tests/testdata/fit_varSD.rda")
+load(file = "tests/testdata/fit_varIT.rda")
+
+test_that("LCx_survFitVarExp", {
+  
+  LCx_varIT <- LCx(fit_varIT, X = 50)
+  LCx_varSD <- LCx(fit_varSD, X = 50)
+  
+  expect_is(LCx_varIT, "list")
+  expect_is(LCx_varSD, "list")
+  
+})
 
 
