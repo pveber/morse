5cff18823bc5aae25b10bfc04878a0a1c0b6c867
vbaudrot <virgile.baudrot@posteo.net>
2018-02-28 17:43:40 +0100

    MFx.survFit.R and plot.MFx.R: update functions

--- R/MFx.survFit.R
+++ R/MFx.survFit.R
@@ -21,9 +21,16 @@
 #'  \eqn{10} for \eqn{MF_{10}}, ...). The default is 50.
 #' @param time_MFx A number giving the time at which  \eqn{MF_{x}} has to be estimated. 
 #' If NULL, the latest time point of the experiment is used.
+#' @param MFx_range A vector of length 2 with lower and upper bound of the 
+#' range of multiplication factor \code{MFx}. The default is a vector \code{c(1, 1000)}.
+#' @param npoints Number of points to compute within \code{MFx_range}. If \code{NULL}, only point computed 
+#' during the adaptation using a binary search in \eqn{O(log n)} are taken into account.
 #' @param mcmc_size Can be used to reduce the number of mcmc samples in order to speed up
 #'  the computation. The default is 1000.
-#' @param MFx_max Upper bound of the multiplication factor. The default is 1000.
+#' @param hb_value If \code{TRUE}, the background mortality \code{hb} is taken into account from the posterior.
+#' If \code{FALSE}, parameter \code{hb} is set to 0. The default is \code{TRUE}.
+#' @param spaghetti If \code{TRUE}, return a set of survival curves using
+#' parameters drawn from the posterior distribution.
 #' @param accuracy Accuracy of the multiplication factor. The default is 0.01.
 #' @param quiet If \code{FALSE}, print the evolution of accuracy.
 #' @param \dots Further arguments to be passed to generic methods
@@ -74,8 +81,11 @@ MFx.survFit <- function(object,
                         profile,
                         x_MFx = 50,
                         time_MFx = NULL,
+                        MFx_range = c(1,1000),
+                        npoints = NULL,
                         mcmc_size = 1000,
-                        MFx_max = 1000,
+                        hb_value = TRUE,
+                        spaghetti = FALSE,
                         accuracy = 0.01,
                         quiet = FALSE, ...){
   
@@ -94,47 +104,119 @@ MFx.survFit <- function(object,
   
   profile$replicate <- rep("predict_MFx", nrow(profile))
   
-  predict_init <- predict(object = object, data_predict = profile, spaghetti = FALSE, mcmc_size = mcmc_size)
+  ls_profile <- list()
+  ls_predict <- list()
   
-  median_backgroundMortality_Conc0 = dplyr::filter(predict_init$df_quantile, time == time_MFx)$q50
-  x_MFx_out = (100 - x_MFx) / 100 * median_backgroundMortality_Conc0
   
-  profile_test = profile
-  profile_test$conc = MFx_max * profile$conc
-  predict_test <- predict(object = object, data_predict = profile_test, spaghetti = FALSE, mcmc_size = mcmc_size)
-  median_Mortality_test = dplyr::filter(predict_test$df_quantile, time == time_MFx)$q50
+  ls_profile[[1]] <- profile
+  ls_predict[[1]] <- predict( object = object,
+                              data_predict = ls_profile[[1]],
+                              spaghetti = spaghetti,
+                              mcmc_size = mcmc_size,
+                              hb_value = hb_value)
   
-  i_round = 0
-  MFx_min = 0 ; MFx_test = MFx_max
-  while(abs(x_MFx_out - median_Mortality_test) > accuracy){
-    if(x_MFx_out - median_Mortality_test < 0){
-      MFx_min = MFx_test
-      MFx_test = MFx_test + (MFx_max - MFx_min)/2
-    }
-    if(x_MFx_out-median_Mortality_test > 0){
-      MFx_max = MFx_test
-      MFx_test = MFx_test - (MFx_max - MFx_min)/2
-    }
-    profile_test$conc = MFx_test * profile$conc
-    predict_test <- predict(object = object, data_predict = profile_test, spaghetti = FALSE, mcmc_size = mcmc_size)
-    median_Mortality_test = dplyr::filter(predict_test$df_quantile, time == time_MFx)$q50
+  filter_time_MFx = dplyr::filter(ls_predict[[1]]$df_quantile, time == time_MFx)
+
+  median_Mortality_test <- filter_time_MFx$q50
+  theoretical_x_MFx <- (100 - x_MFx) / 100 * filter_time_MFx$q50 # Necessary to compared with accuracy
+  
+  if(is.null(npoints)){
+    #
+    # binary search of MFx in O(log n)
+    #
+    i = 1
+    MFx = 1
+    MFx_min = min(MFx_range)
+    MFx_test = max(MFx_range)
     
-    if(quiet == FALSE){
-      i_round = i_round +1
-      cat(i_round,"accuracy:", abs(x_MFx_out - median_Mortality_test), " with multiplication factor:",  MFx_test, "\n")
+    while(abs(theoretical_x_MFx - median_Mortality_test) > accuracy){
+    
+      MFx = c(MFx, MFx_test)
+        
+      ls_profile[[i+1]] <- profile
+      ls_profile[[i+1]]$conc <- MFx_test * profile$conc
+      
+      ls_predict[[i+1]] <- predict(object = object,
+                                  data_predict = ls_profile[[i+1]],
+                                  spaghetti = spaghetti,
+                                  mcmc_size = mcmc_size,
+                                  hb_value = hb_value)
+      
+      filter_time_MFx = dplyr::filter(ls_predict[[i+1]]$df_quantile, time == time_MFx)
+      median_Mortality_test = filter_time_MFx$q50
+      df_MFx <- dplyr::add_row(df_MFx,
+                               MFx = MFx_test,
+                               q50 = filter_time_MFx$q50,
+                               qinf95 = filter_time_MFx$qinf95,
+                               qsup95 = filter_time_MFx$qsup95 )
+      
+      
+      
+      if(quiet == FALSE){
+        cat(i,"accuracy:", abs(theoretical_x_MFx - median_Mortality_test), " with multiplication factor:",  MFx_test, "\n")
+      }
+      
+      i = i + 1
+      if(theoretical_x_MFx - median_Mortality_test < 0){
+        MFx_min = MFx_test
+        MFx_test = MFx_test + (MFx_max - MFx_min)/2
+      }
+      if(theoretical_x_MFx - median_Mortality_test > 0){
+        MFx_max = MFx_test
+        MFx_test = MFx_test - (MFx_max - MFx_min)/2
+      }
     }
+    k <- 1:length(MFx)
+  }
+  if(!is.null(npoints)){
+    
+    MFx = seq(min(MFx_range), max(MFx_range), length.out = npoints)
+    
+    k <- 1:npoints
+
+    ls_profile <- lapply(k, function(kit){
+      profil_test <- profile
+      profil_test$conc <- MFx[kit] * profile$conc
+      return(profil_test)
+    })
+    
+    ls_predict <- lapply(k, function(kit){
+      predict(object = object,
+              data_predict = ls_profile[[kit]],
+              spaghetti = spaghetti,
+              mcmc_size = mcmc_size,
+              hb_value = hb_value)
+    })
+    
   }
   
-  ls_out = list(Multiplication_factor = MFx_test,
+  ls_predict_quantile <- lapply(k, function(kit){
+    df_quantile <- ls_predict[[kit]]$df_quantile
+    df_quantile$MFx <- rep(MFx[[kit]], nrow(ls_predict[[kit]]$df_quantile))
+    return(df_quantile)
+  })
+  
+  df_ls_predict <- do.call("rbind", ls_predict_quantile)
+  
+  df_MFx <- dplyr::filter(df_ls_predict, time == time_MFx) %>%
+    mutate(accuracy = abs(theoretical_x_MFx - q50)) %>%
+    mutate(optimal_MFx = ifelse(accuracy == min(accuracy), MFx, NA))
+
+  optimal_MFx <- dplyr::filter(df_MFx, !is.na(optimal_MFx))
+  
+  ls_out = list(optimal_MFx = optimal_MFx,
                 x_MFx = x_MFx,
                 time_MFx = time_MFx,
-                initial_x_MFx = median_backgroundMortality_Conc0,
-                theoretical_x_MFx = x_MFx_out,
-                final_x_MFx = median_Mortality_test,
-                initial_prediction = predict_init,
-                final_prediction = predict_test)
+                initial_x_MFx = df_MFx$q50,
+                theoretical_x_MFx = theoretical_x_MFx,
+                df_MFx = df_MFx, # MFx at specific time
+                ls_predict = ls_predict,
+                df_ls_predict = df_ls_predict,
+                ls_profile = ls_profile)
   
-  class(ls_out) = c("MFx", "list")
+  class(ls_out) = c("list", "MFx")
   
   return(ls_out)
 }
+
+
--- R/plot.MFx.R
+++ R/plot.MFx.R
@@ -5,7 +5,10 @@
 #'
 #'
 #' @param x An object of class \code{MFx}.
-#' @param xlab A label for the \eqn{X}-axis, by default \code{Time}.
+#' @param x_variable A character to define the variable for the \eqn{X}-axis,
+#'  either \code{"MFx"} or \code{"Time"}. The default is \code{"MFx"}.
+#' @param xlab A label for the \eqn{X}-axis, by default \code{NULL} and depend on the
+#' argument \code{x_variable}.
 #' @param ylab A label for the \eqn{Y}-axis, by default \code{Survival rate median and 95 CI}.
 #' @param main A main title for the plot.
 #' @param \dots Further arguments to be passed to generic methods.
@@ -36,48 +39,98 @@
 #'
 #'
 plot.MFx <- function(x,
-                     xlab = "Time",
+                     spaghetti = FALSE,
+                     x_variable = "MFx", # other option is "Time"
+                     xlab = NULL,
                      ylab = "Survival rate \n median and 95 CI",
-                     main = NULL, ...){
+                     main = NULL,
+                     log_scale = FALSE,
+                     ...){
   
-  Multiplication_factor = x$Multiplication_factor
-  time_MFx = x$time_MFx
-  initial_x_MFx = x$initial_x_MFx
-  theoretical_x_MFx = x$theoretical_x_MFx
-  final_x_MFx = x$final_x_MFx
-  initial_prediction = x$initial_prediction
-  final_prediction = x$final_prediction
+  # definition of xlab and check x_variable
+  if(is.null(xlab)){
+    if(x_variable == "MFx"){
+      xlab = "Multiplication Factor"
+    } else if(x_variable == "Time"){
+      xlab = "Time"
+    } else stop("the argument 'x_variable' must be 'MFx' or 'Time'. The default is 'MFx'.")
+  }
   
   if(is.null(main)){
-    main <- paste("Multiplication Factor-Response curve: MF is", 
-                  round(Multiplication_factor, digits = 3),
-                  "(surv. rate=", round(final_x_MFx, digits = 2), 
-                  "; t=", round(time_MFx, digits = 2), ")")
+    main <- paste("Multiplication Factor MF is", 
+                  round(x$optimal_MFx$MFx, digits = 3))
   } 
   
-  # Plot
+  # Load data frame from x
+  if(x_variable == "MFx"){
+    
+    df_MFx <- x$df_MFx
+    
+    legend.point = data.frame(
+      x.pts = rep(x$optimal_MFx$MFx, 3),
+      y.pts = c(x$optimal_MFx$q50, x$optimal_MFx$qinf95, x$optimal_MFx$qsup95),
+      pts.leg = c(paste("median: ", round(x$optimal_MFx$q50,digits = 2)),
+                  paste("quantile 2.5%: ", round(x$optimal_MFx$qinf95, digits = 2)),
+                  paste("quantile 97.5%: ", round(x$optimal_MFx$qsup95, digits = 2)))
+    )
+    
+    
+    MFx_plt <- ggplot() + theme_bw() +
+      labs(title = main) +
+      scale_x_continuous(name = xlab) +
+      scale_y_continuous(name = ylab, limits = c(0,1)) +
+      # Initial prediction:
+      geom_ribbon(data = df_MFx,
+                  aes(x = MFx, ymin = qinf95, ymax = qsup95),
+                  fill = "lightgrey") +
+      geom_line(data = df_MFx,
+                aes(x = MFx, y = q50), color = "orange") +
+      geom_vline(xintercept = x$optimal_MFx$MFx, col="grey70", linetype=2) +
+      geom_point(data = legend.point,
+                 aes(x = x.pts, y=y.pts,
+                     color= pts.leg)) +
+      scale_color_manual(values=c("orange", "black", "black"))
+    
+    if(log_scale == TRUE){
+      MFx_plt <- MFx_plt +
+        scale_x_log10()
+    }
+    
+  }
   
-  MFx_plt <- ggplot() + theme_bw() +
-    labs(title = main) +
-    scale_x_continuous(name = xlab) +
-    scale_y_continuous(name = ylab, limits = c(0,1)) +
-    # Initial prediction:
-    geom_ribbon(data = initial_prediction$df_quantile,
-                aes(x = time, ymin = qinf95, ymax = qsup95),
-                fill = "grey80", alpha = 0.4) +
-    geom_line(data = initial_prediction$df_quantile,
-              aes(x = time, y = q50),
-              col="orange", size = 0.3) +
-    # Final prediction:
-    geom_ribbon(data = final_prediction$df_quantile,
-                aes(x = time, ymin = qinf95, ymax = qsup95),
-                fill = "grey30", alpha = 0.4) +
-    geom_line(data = final_prediction$df_quantile,
-              aes(x = time, y = q50),
-              col="orange", size = 1) +
-    geom_segment(aes(x = time_MFx, y = initial_x_MFx,
-                     xend = time_MFx, yend = final_x_MFx),
-                 arrow = arrow(length = unit(0.2,"cm")))
+  if(x_variable == "Time"){
+    
+    initial_prediction <- dplyr::filter(x$df_ls_predict, MFx == 1)
+    final_prediction <- dplyr::filter(x$df_ls_predict, MFx == x$optimal_MFx$MFx)
+    
+    y_arrow <- dplyr::filter(initial_prediction, time == x$time_MFx)$q50
+    yend_arrow <- dplyr::filter(final_prediction, time == x$time_MFx)$q50
+    
+    # Plot
+    MFx_plt <- ggplot() + theme_bw() +
+      labs(title = main) +
+      scale_x_continuous(name = xlab) +
+      scale_y_continuous(name = ylab, limits = c(0,1)) +
+      # Initial prediction:
+      geom_ribbon(data = initial_prediction,
+                  aes(x = time, ymin = qinf95, ymax = qsup95),
+                  fill = "grey80", alpha = 0.4) +
+      geom_line(data = initial_prediction,
+                aes(x = time, y = q50),
+                col="orange", size = 0.3) +
+      # Final prediction:
+      geom_ribbon(data = final_prediction,
+                  aes(x = time, ymin = qinf95, ymax = qsup95),
+                  fill = "grey30", alpha = 0.4) +
+      geom_line(data = final_prediction,
+                aes(x = time, y = q50),
+                col="orange", size = 1) +
+      geom_segment(aes(x = x$time_MFx, y = y_arrow,
+                       xend = x$time_MFx, yend =  yend_arrow),
+                   arrow = arrow(length = unit(0.2,"cm")))
+    
+  }
+  
   
   return(MFx_plt)
   
