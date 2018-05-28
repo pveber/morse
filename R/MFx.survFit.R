c62c41cebe6cf3f7e9423671d99b8883b2d69c43
vbaudrot <virgile.baudrot@posteo.net>
2018-05-28 10:47:26 +0200

    MFx.survFit.R: improve documentation

--- R/MFx.survFit.R
+++ R/MFx.survFit.R
@@ -78,8 +78,12 @@
 #' # (4) data to predict
 #' data_4prediction <- data.frame(time = 1:10, conc = c(0,0.5,3,3,0,0,0.5,3,1.5,0))
 #' 
-#' # (5) estimate MF for 30% reduction of survival at time 4
+#' # (5) estimate MF(x=30, t=4), that is for 30% reduction of survival at time 4
 #' MFx_SD_10.4 <- MFx(out_SD, data_predict = data_4prediction , X = 30, time_MFx = 4)
+#' 
+#' # (5bis) estimate MF(x,t) along the MF_range from 5 to 10 (50) (X = NULL)
+#' MFx_SD_range <- MFx(out_SD, data_predict = data_4prediction ,
+#'                     X = NULL, time_MFx = 4, MFx_range = seq(5, 10, length.out = 50))
 #' }
 #' 
 #' 
