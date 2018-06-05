9f04a68fa3fc793dd16e737e58d34cf704a0185f
vbaudrot <virgile.baudrot@posteo.net>
2018-06-05 15:20:27 +0200

    plot.MFx: change name in example

--- R/plot.MFx.R
+++ R/plot.MFx.R
@@ -34,16 +34,16 @@
 #' data_4prediction <- data.frame(time = 1:10, conc = c(0,0.5,3,3,0,0,0.5,3,1.5,0))
 #' 
 #' # (5) estimate MF for 30% reduction of survival at time 4
-#' MFx_SD_10.4 <- MFx(out_SD, data_predict = data_4prediction , X = 30, time_MFx = 4)
+#' MFx_SD_30.4 <- MFx(out_SD, data_predict = data_4prediction , X = 30, time_MFx = 4)
 #' 
 #' # (6) plot the object of class 'MFx'
-#' plot(MFx_SD_10.4)
+#' plot(MFx_SD_30.4)
 #' 
 #' # (6bis) plot with log-scale of x-axis
-#' plot(MFx_SD_10.4, log_scale = TRUE)
+#' plot(MFx_SD_30.4, log_scale = TRUE)
 #' 
 #' # (6ter) plot with "Time" as the x-axis
-#' plot(MFx_SD_10.4, x_variable = "Time") 
+#' plot(MFx_SD_30.4, x_variable = "Time") 
 #' 
 #' # (7) plot when X = NULL and along a MFx_range from 5 to 10:
 #' MFx_SD_range <- MFx(out_SD, data_predict = data_4prediction ,
